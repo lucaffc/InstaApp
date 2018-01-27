d796b870f0f13721810f6518a638b76fd7ecaec7
lucaffc <l.lucassouza@hotmail.com>
2018-01-27 01:18:25 -0200

    Alteração do post js, inclusão da iteração do botão like

--- src/components/Post.js
+++ src/components/Post.js
@@ -30,13 +30,48 @@ export default class Post extends Component<{}> {
   carregaIcone(likeada){
     return likeada ? require('../../resources/img/s2-checked.png'):
     require('../../resources/img/s2.png')
+  }
 
+  exibeLikes(likers){
+    if(likers.length <= 0)
+      return;
+      return(
+      <Text style={styles.likes}>
+      {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
+      </Text>
+    );
+  }
+
+  exibeLegenda(foto){
+    if(foto.comentario === '')
+      return;
+      return (
+    <View style={styles.comentario}>
+      <Text style={styles.tituloComentario}> {foto.loginUsuario} </Text>
+      <Text> {foto.comentario} </Text>
+    </View>
+    );
   }
 
   like(){
+
+    const { foto } = this.state;
+    let novaLista = []
+    if(!foto.likeada){
+      novaLista = [
+        ...foto.likers,
+        {login: 'meuUsuario'}
+      ]
+    } else{
+      novaLista = foto.likers.filter(liker => {
+        return liker.login !== 'meuUsuario'
+      })
+    }
+
     const fotoAtualizada = {
-      ...this.state.foto,
-      likeada: !this.state.foto.likeada
+      ...foto,
+      likeada: !foto.likeada,
+      likers: novaLista
     }
     this.setState({foto: fotoAtualizada})
   }
@@ -51,14 +86,16 @@ export default class Post extends Component<{}> {
             <Text>{foto.loginUsuario}</Text>
           </View>
 
-            <Image source={{uri: foto.urlFoto}}
-                style={styles.foto}/>
-
-        <View style={styles.rodape}>
-          <TouchableOpacity onPress={this.like.bind(this)}>
+          <Image source={{uri: foto.urlFoto}}
+              style={styles.foto}/>
+          <View style={styles.rodape}>
+            <TouchableOpacity onPress={this.like.bind(this)}>
           <Image style={styles.botaoDeLike}
              source={this.carregaIcone(foto.likeada)} />
           </TouchableOpacity>
+
+          {this.exibeLikes(foto.likers)}
+          {this.exibeLegenda(foto)}
         </View>
     </View>
     )
@@ -85,11 +122,21 @@ const styles = StyleSheet.create({
     height: width
   },
   botaoDeLike: {
+    marginBottom: 10,
     height:40,
     width:40
   },
   rodape:{
     margin:10
+  },
+  likes: {
+    fontWeight: 'bold'
+  },
+  comentario:{
+    flexDirection: 'row'
+  },
+  tituloComentario:{
+    fontWeight: 'bold',
+    marginRight: 5
   }
-}
-);
+});
